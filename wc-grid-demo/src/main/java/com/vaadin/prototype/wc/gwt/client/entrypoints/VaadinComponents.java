0246daaf536e8a2814b0c1c54f4547bfa5d6b539
Artur Signell <artur@vaadin.com>
2015-01-23 01:06:14 +0200

    Disable uncaught exception handler in production
    Using it hides the real location of exceptions and makes
    debugging much harder
    
    Change-Id: Ie59f126bd495ed8c22a35861df92ed44c7a3d173

--- wc-grid-demo/src/main/java/com/vaadin/prototype/wc/gwt/client/entrypoints/VaadinComponents.java
+++ wc-grid-demo/src/main/java/com/vaadin/prototype/wc/gwt/client/entrypoints/VaadinComponents.java
@@ -21,7 +21,9 @@ import com.vaadin.prototype.wc.gwt.client.widgets.WCVSlider;
 public class VaadinComponents implements EntryPoint {
 
     public void onModuleLoad() {
-        GWT.setUncaughtExceptionHandler(new SuperDevModeUncaughtExceptionHandler());
+    	if (!GWT.isProdMode()) {
+    		GWT.setUncaughtExceptionHandler(new SuperDevModeUncaughtExceptionHandler());
+    	}
 
         // load vaadin-theme if specified in body
         WCUtils.loadVaadinGlobalTheme(new Function() {
