c76f5518656f2e8285cde58101711bf4f57a87c0
Manolo Carrasco <manolo@vaadin.com>
2015-03-06 20:09:14 +0100

    Refactor execution of js datasource functions
    Change-Id: Ib2d9ef69e6093167a690b478a2412051ede6104f

--- vaadin-components-gwt/src/main/java/com/vaadin/components/grid/data/GridJsFuncDataSource.java
+++ vaadin-components-gwt/src/main/java/com/vaadin/components/grid/data/GridJsFuncDataSource.java
@@ -1,25 +1,26 @@
 package com.vaadin.components.grid.data;
 
-import static com.google.gwt.query.client.GQuery.$$;
+import static com.google.gwt.query.client.GQuery.$;
+import static com.google.gwt.query.client.GQuery.console;
+import static com.google.gwt.query.client.GQuery.window;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArrayMixed;
+import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.js.JsUtils;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.vaadin.components.grid.GridComponent;
 
 /**
  * Datasource where requestRows() is delegated to a js native function
- *
- * @author manolo
  */
 public class GridJsFuncDataSource extends GridDataSource {
-    private JavaScriptObject f;
+
+    private JavaScriptObject jsFunction;
 
     public GridJsFuncDataSource(JavaScriptObject jso, int rows, GridComponent grid) {
         super(grid);
         assert JsUtils.isFunction(jso);
-        f = jso;
+        jsFunction = jso;
         size = rows;
     }
 
@@ -28,17 +29,19 @@ public class GridJsFuncDataSource extends GridDataSource {
             final int firstRowIndex,
             final int numberOfRows,
             final com.vaadin.client.data.AbstractRemoteDataSource.RequestRowsCallback<JsArrayMixed> callback) {
-        JsArrayMixed o = exec(f, firstRowIndex, numberOfRows,
-                new AsyncCallback<JsArrayMixed>() {
-                    public void onFailure(Throwable caught) {
-                    }
 
-                    public void onSuccess(JsArrayMixed result) {
-                        setRowData(firstRowIndex, result);
-                    }
-                });
-        if (o != null) {
-            setRowData(firstRowIndex, o);
+        JavaScriptObject jsCallback = JsUtils.wrapFunction(new Function() {
+            public void f() {
+                JsArrayMixed result = arguments(0);
+                setRowData(firstRowIndex, result);
+            }
+        });
+
+        // When we call the native function, it could return the array of rows directly
+        // or via the provided callback function.
+        JsArrayMixed result = JsUtils.jsni(jsFunction, "call", jsFunction, firstRowIndex, numberOfRows, jsCallback);
+        if (result != null) {
+            setRowData(firstRowIndex, result);
         }
     }
 
@@ -46,12 +49,4 @@ public class GridJsFuncDataSource extends GridDataSource {
     public Object getRowKey(JsArrayMixed row) {
         return JsUtils.JSON2String(row);
     }
-
-    private native JsArrayMixed exec(JavaScriptObject f, int idx,
-            int count, AsyncCallback<JsArrayMixed> cb)
-   /*-{
-       return f(idx, count, function(r) {
-           cb.@com.google.gwt.user.client.rpc.AsyncCallback::onSuccess(*)(r);
-       });
-   }-*/;
 }
