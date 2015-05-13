f13f593e801f595a42fe0dd546920c3025af8b38
Sauli Tähkäpää <sauli@vaadin.com>
2015-05-13 14:40:09 +0300

    Update visual-tests to test the overview look.
    Change-Id: I229ec9caeb61692a9eae602cdff0fa6c4638694d

--- visual-test/vaadin-grid/test.js
+++ visual-test/vaadin-grid/test.js
@@ -3,7 +3,20 @@ var gemini = require('gemini');
 gemini.suite('vaadin-grid', function(rootSuite) {
   rootSuite.setUrl('/visual-test/vaadin-grid/');
 
-  gemini.suite('headers', function(suite) {
-    suite.setCaptureElements('th').capture('first column');
+  rootSuite.before(function(actions, find) {
+    actions.waitForJSCondition(function(window) {
+      var grid = window.document.querySelector('v-grid');
+
+      if(grid._grid) {
+        return grid._grid.isWorkPending() === false;
+      }
+
+      return false;
+
+    }, 5000);
+  });
+
+  gemini.suite('overview', function(suite) {
+    suite.setCaptureElements('v-grid').capture('default');
   });
 });
