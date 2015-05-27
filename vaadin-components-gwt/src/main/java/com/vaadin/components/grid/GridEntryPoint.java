3e0297c5d39881bd07056aac258fd6d2cfe86a68
Taras Hupalo <taras.hupalo@gmail.com>
2015-05-27 11:06:31 +0300

    Remove browser specific selectors from the body element
    Change-Id: I2f38067cc33fc2c2c8e0e2a311a43213f73ad3c8

--- vaadin-components-gwt/src/main/java/com/vaadin/components/grid/GridEntryPoint.java
+++ vaadin-components-gwt/src/main/java/com/vaadin/components/grid/GridEntryPoint.java
@@ -1,6 +1,9 @@
 package com.vaadin.components.grid;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.vaadin.client.BrowserInfo;
 
 /**
  * Exports vaadin grid.
@@ -9,5 +12,11 @@ public class GridEntryPoint implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
+        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+            @Override
+            public void execute() {
+                RootPanel.get().removeStyleName(BrowserInfo.get().getCSSClass());
+            }
+        });
     }
 }
