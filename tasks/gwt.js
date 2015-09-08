3a49a5cc26992b95ffab84978b0aadbf24d71530
Manolo Carrasco <manolo@vaadin.com>
2015-09-09 00:57:47 +0200

    Fix gwt tasks after moving files to root and remove unused ones

--- tasks/gwt.js
+++ tasks/gwt.js
@@ -17,7 +17,7 @@ var component = 'vaadin-grid';
 var moduleName = 'VaadinGrid';
 var webDir = 'java/src/main/webapp/';
 var webComponentDir = webDir + component + '/';
-var componentDir = 'vaadin-components/' + component;
+var componentDir = './';
 
 function system(command, cb) {
   cmd.exec(command, function(err, stdout, stderr) {
@@ -48,8 +48,6 @@ gulp.task('gwt:compile', ['gwt:clean-maven'], function(done) {
   });
 });
 
-gulp.task('gwt:copy', ['gwt:copy-files', 'gwt:copy-imports', 'gwt:copy-deferred']);
-
 gulp.task('gwt:clean-maven', function(done) {
   if(args.gwtSkipClean) {
     done();
@@ -66,12 +64,6 @@ gulp.task('test:gwt', function(done) {
   });
 });
 
-gulp.task('watch:gwt', function() {
-  var paths = ['java/src/main/webapp/vaadin-grid/**/*'];
-
-  gulp.watch(paths, ['gwt']);
-});
-
 gulp.task('clean:gwt', ['gwt:clean-maven'], function() {
   if(args.gwtSkipClean) {
     return;
@@ -81,25 +73,10 @@ gulp.task('clean:gwt', ['gwt:clean-maven'], function() {
   fs.mkdirsSync(componentDir);
 });
 
-gulp.task('gwt:copy-files', ['gwt:compile', 'clean:gwt'], function() {
-  return gulp.src([webComponentDir + '**/*', '!' + webComponentDir + 'sdm.js'])
-          .pipe(replace(new RegExp('^.*script.*\.\./\.\..*' + moduleName + '.*'+ moduleName +'.*$','mg'), '  <link rel="import" href="../'+component+'.html">'))
-          .pipe(replace(new RegExp('^.*script.*\.\..*' + moduleName + '.*'+ moduleName +'.*$','mg'), '  <link rel="import" href="'+component+'.html">'))
-          .pipe(replace(/(src|href)=("|')([\.\.\/]*)\/bower_components\//mg, '$1=$2$3/../bower_components/'))
-          .pipe(replace(/^.*src="[\.\/]*sdm.js".*$/mg, ''))
-          .pipe(replace(/^.*<!-- .*(<.*vaadin-grid.min.js.*) -->/mg, '$1'))
-          .pipe(gulp.dest(componentDir));
-});
-
-gulp.task('gwt:copy-imports', ['gwt:compile', 'clean:gwt'], function() {
+gulp.task('gwt:copy', ['gwt:compile', 'clean:gwt'], function() {
   var warDir = 'java/target/vaadin-grid-' + version + '/';
   var modulePath = warDir + moduleName + 'Import/';
   return gulp.src(modulePath + moduleName +'Import.nocache.js')
           .pipe(rename(component + '.min.js'))
-          .pipe(gulp.dest(componentDir));
-});
-
-gulp.task('gwt:copy-deferred', ['gwt:compile', 'clean:gwt'], function() {
-  return gulp.src(componentDir + 'deferred')
           .pipe(gulp.dest(componentDir));
 });
