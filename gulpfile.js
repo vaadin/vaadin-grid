6e83736e22f65b759619c68409831239b014e917
Sauli Tähkäpää <sauli@vaadin.com>
2015-10-13 09:38:44 +0300

    Split tests into desktop and mobile. Removed unused tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var common = require('./tasks/common');
 require('web-component-tester').gulp.init(gulp);
 var args = require('yargs').argv;
 
-var version = '0.3.0';
-
 gulp.task('default', function() {
   console.log('\n  Use:\n    gulp <clean|gwt[ --gwt-pretty]|test[:validation:sauce]>\n');
 });
@@ -17,63 +15,23 @@ gulp.task('gwt', ['gwt:copy']);
 
 gulp.task('test', ['gwt:validate', 'test:local']);
 
-gulp.task('test:validation', function(done) {
-  common.test(
-    {
-      browserOptions: {
-        name: common.localAddress() + ' / ' + new Date(),
-        build: 'vaadin-components / validation'
-      },
-      plugins: {
-        sauce: {
-          username: args.sauceUsername,
-          accessKey: args.sauceAccessKey,
-          browsers: [
-            'Windows 7/chrome@41',
-            'Windows 7/firefox@36',
-            'Windows 7/internet explorer@11'
-          ]
-        },
-        'teamcity-reporter': args.teamcity
-      },
-      root: '.',
-      webserver: {
-        port: 2000,
-        hostname: common.localAddress()
-      }
-    }, done);
-});
-
-gulp.task('test:vaadin', function(done) {
-  common.test(
-    {
-      browserOptions: {
-        url: 'http://validation-hub.devnet.vaadin.com:4444/wd/hub'
-      },
-      activeBrowsers: [
-        {
-          browserName: "chrome",
-          version: "40",
-          platform: "VISTA"
-        }
-      ],
-      root: '.',
-      webserver: {
-        port: 2000,
-        hostname: common.localAddress()
-      }
-    }, done);
-});
-
-gulp.task('test:sauce', function(done) {
+gulp.task('test:desktop', function(done) {
   common.testSauce(
     [],
-    ['Windows 7/chrome@41',
-      'Windows 7/firefox@36',
-      'Windows 7/internet explorer@11',
-      'OS X 10.10/safari@8.0',
-      'OS X 10.10/iphone@8.1',
-      'Linux/android@5.0'],
-    'vaadin-components / ' + version,
+    ['Windows 10/chrome@45',
+      'Windows 10/firefox@41',
+      'Windows 10/internet explorer@11',
+      //'Windows 10/microsoftedge@20',
+      'OS X 10.10/safari@8.0'],
+    'vaadin-grid',
      done)
+});
+
+gulp.task('test:mobile', function(done) {
+  common.testSauce(
+    [],
+    ['OS X 10.10/iphone@9',
+      'Linux/android@5.1'],
+    'vaadin-grid',
+    done)
 });
