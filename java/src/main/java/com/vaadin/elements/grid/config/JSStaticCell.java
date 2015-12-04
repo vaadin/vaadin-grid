1cc5773803499b6b7be55c196b85b7b791fc8438
Manolo Carrasco <manolo@vaadin.com>
2015-12-04 17:45:44 +0200

    Move static-cell binding logic, makes code more readable

--- java/src/main/java/com/vaadin/elements/grid/config/JSStaticCell.java
+++ java/src/main/java/com/vaadin/elements/grid/config/JSStaticCell.java
@@ -1,23 +1,57 @@
 package com.vaadin.elements.grid.config;
 
+import static com.vaadin.shared.ui.grid.GridStaticCellType.HTML;
+import static com.vaadin.shared.ui.grid.GridStaticCellType.WIDGET;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
+
+import com.vaadin.client.widgets.Grid.StaticSection.StaticCell;
+import com.vaadin.elements.grid.table.GridStaticSection;
 
 /**
  * This class is a JsInterop wrapper for the JS object representing a grid
  * header/footer cell configuration
  */
-@JsType(isNative=true)
-public interface JSStaticCell {
-    @JsProperty Object getContent();
+@JsType()
+public class JSStaticCell {
 
-    @JsProperty void setContent(Object content);
+    private GridStaticSection section;
+    private StaticCell cell;
+    private Object content;
 
-    @JsProperty int getColspan();
+    public JSStaticCell(GridStaticSection staticSection, StaticCell staticCell) {
+        section = staticSection;
+        cell = staticCell;
+        content = cell.getType() == HTML ? cell.getHtml()
+                : cell.getType() == WIDGET ? cell.getWidget().getElement()
+                        : !"".equals(cell.getText()) ? cell.getText() : null;
+    }
 
-    @JsProperty void setColspan(int colspan);
+    @JsProperty public Object getContent() {
+        return content;
+    }
 
-    @JsProperty String getClassName();
+    @JsProperty public void setContent(Object content) {
+        this.content = content;
+        section.contentChanged(content, cell);
+        section.cellChanged(cell);
+    }
 
-    @JsProperty void setClassName(String s);
+    @JsProperty public int getColspan() {
+        return cell.getColspan();
+    }
+
+    @JsProperty public void setColspan(int colspan) {
+        cell.setColspan(colspan);
+        section.cellChanged(cell);
+    }
+
+    @JsProperty public String getClassName() {
+        return cell.getStyleName();
+    }
+
+    @JsProperty public void setClassName(String className) {
+        cell.setStyleName(className);
+        section.cellChanged(cell);
+    }
 }
