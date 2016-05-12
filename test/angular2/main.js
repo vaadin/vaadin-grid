ed179e22c863006dc6effd10c00e01778727fa4e
Sauli Tähkäpää <sauli@vaadin.com>
2016-05-12 14:02:11 +0300

    Use generic directive from angular2-polymer

--- test/angular2/main.js
+++ test/angular2/main.js
@@ -1,6 +1,4 @@
-System.register(['@angular/platform-browser-dynamic', '@angular/core', '../../directives/vaadin-grid'], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register(['@angular/platform-browser-dynamic', '@angular/core', '@vaadin/angular2-polymer'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -10,7 +8,7 @@ System.register(['@angular/platform-browser-dynamic', '@angular/core', '../../di
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var platform_browser_dynamic_1, core_1, vaadin_grid_1;
+    var platform_browser_dynamic_1, core_1, angular2_polymer_1;
     var TestApp;
     return {
         setters:[
@@ -20,10 +18,11 @@ System.register(['@angular/platform-browser-dynamic', '@angular/core', '../../di
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (vaadin_grid_1_1) {
-                vaadin_grid_1 = vaadin_grid_1_1;
+            function (angular2_polymer_1_1) {
+                angular2_polymer_1 = angular2_polymer_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             TestApp = (function () {
                 function TestApp(e, ref) {
                     this.items = [];
@@ -33,21 +32,29 @@ System.register(['@angular/platform-browser-dynamic', '@angular/core', '../../di
                 TestApp.prototype.detectChanges = function () {
                     this._ref.detectChanges();
                 };
-                TestApp.prototype.onGridReady = function (grid) {
-                    grid.columns[0].flex = '2';
-                    var event = new CustomEvent('readyForTests', { detail: this });
-                    this._host.dispatchEvent(event);
+                TestApp.prototype.ngAfterViewInit = function () {
+                    var _this = this;
+                    var grid = this.grid.nativeElement;
+                    grid.then(function () {
+                        grid.columns[0].flex = '2';
+                        var event = new CustomEvent('readyForTests', { detail: _this });
+                        _this._host.dispatchEvent(event);
+                    });
                 };
+                __decorate([
+                    core_1.ViewChild('grid'), 
+                    __metadata('design:type', Object)
+                ], TestApp.prototype, "grid", void 0);
                 TestApp = __decorate([
                     core_1.Component({
                         selector: 'test-app',
-                        template: "\n    <vaadin-grid (grid-ready)=\"onGridReady($event)\">\n      <table>\n        <colgroup>\n          <col>\n          <col>\n          <col *ngIf=\"thirdColumn\" />\n        </colgroup>\n        <tbody>\n          <tr *ngFor=\"let item of items\">\n            <td>foo</td><td>bar</td>\n          </tr>\n        </tbody>\n      </table>\n    </vaadin-grid>\n    ",
-                        directives: [vaadin_grid_1.VaadinGrid]
+                        template: "\n    <vaadin-grid #grid>\n      <table>\n        <colgroup>\n          <col>\n          <col>\n          <col *ngIf=\"thirdColumn\" />\n        </colgroup>\n        <tbody>\n          <tr *ngFor=\"let item of items\">\n            <td>foo</td><td>bar</td>\n          </tr>\n        </tbody>\n      </table>\n    </vaadin-grid>\n    ",
+                        directives: [angular2_polymer_1.PolymerElement('vaadin-grid')]
                     }), 
                     __metadata('design:paramtypes', [core_1.ElementRef, core_1.ChangeDetectorRef])
                 ], TestApp);
                 return TestApp;
-            }());
+            })();
             exports_1("TestApp", TestApp);
             document.body.addEventListener('bootstrap', function () {
                 platform_browser_dynamic_1.bootstrap(TestApp);
@@ -55,5 +62,4 @@ System.register(['@angular/platform-browser-dynamic', '@angular/core', '../../di
         }
     }
 });
-
 //# sourceMappingURL=main.js.map
