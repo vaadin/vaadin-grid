04986b59c4263f0f3120b9b39067f1e823a62d47
Manolo Carrasco <manolo@vaadin.com>
2016-08-31 15:21:43 +0200

    Implement custom event detail.
    Now the user can say the grid to listen to certain events and
    fire a custom event that contains the associated data of the
    element which recives the original event.
    
    It should work in either closed and opened shadow.
    
    Example:
    <vaadin-grid demo detailed-events='["contextmenu"]'>
    
    grid.addEventListener('detailed-event', e => {
       e.detail.event.preventDefault();
       console.log(e.detail.row, ev.detail.column, ev.detail.data);
    });

---
+++ java/src/main/java/com/vaadin/elements/grid/config/JSDetail.java
@@ -0,0 +1,25 @@
+package com.vaadin.elements.grid.config;
+
+import com.google.gwt.dom.client.NativeEvent;
+
+import com.vaadin.elements.common.js.JS;
+
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ * This class is a JsInterop wrapper for the JS object representing detailed event.
+ */
+@JsType(namespace = JS.NAMESPACE_API)
+public class JSDetail {
+    @JsProperty
+    public double row;
+    @JsProperty
+    public double column;
+    @JsProperty
+    public String section;
+    @JsProperty
+    public NativeEvent event;
+    @JsProperty
+    public Object data;
+}
