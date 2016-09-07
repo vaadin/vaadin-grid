bd0bd115e3f27668e75fb93b6f03cb1db9f5e823
Anton Platonov <platosha@gmail.com>
2016-09-07 18:00:39 +0300

    v1.1.2

--- tasks/gwt.js
+++ tasks/gwt.js
@@ -12,7 +12,7 @@ var rl = require('readline');
 var watch = require('gulp-watch');
 
 var gwtproject = 'java';
-var version = '0.3.0';
+var version = '1.1.2';
 var major = version.replace(/(\d+\.\d+\.).*$/, '$1');
 
 var gwtNocacheJs = 'java/target/vaadin-grid-' + version + '/VaadinGridImport/VaadinGridImport.nocache.js';
