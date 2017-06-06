ea965bba4eb468ca7533ea78c8099e912c00e788
Tomi Virkki <virkki@vaadin.com>
2017-06-06 13:27:20 +0300

    Test Vulcanize/Closure compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var htmlExtract = require('gulp-html-extract');
-var stylelint = require('gulp-stylelint');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const htmlExtract = require('gulp-html-extract');
+const stylelint = require('gulp-stylelint');
+const vulcanize = require('gulp-vulcanize');
+const crisper = require('gulp-crisper');
+const closure = require('google-closure-compiler-js').gulp();
+const fs = require('fs');
+const filter = require('gulp-filter');
 
 gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
 
@@ -46,4 +51,29 @@ gulp.task('lint:css', function() {
       {formatter: 'string', console: true}
     ]
   }));
+});
+
+gulp.task('compile', function() {
+  const excludes = fs.readdirSync('bower_components').map((dependency) => {
+    return '../' + dependency + '/';
+  });
+  const externsPath =
+      'node_modules/google-closure-compiler-js/contrib/externs/polymer-1.0.js';
+  const externsSrc = fs.readFileSync(externsPath, 'utf-8');
+  return gulp.src('*.html')
+    .pipe(vulcanize({
+      excludes: excludes
+    }))
+    .pipe(crisper({onlySplit: true}))
+    .pipe(filter('*.js'))
+    .pipe(closure({
+      polymerPass: true,
+      externs: [
+        {'src': externsSrc, 'path': 'polymer-1.0.js'}
+      ],
+      compilationLevel: 'SIMPLE',
+      warningLevel: 'VERBOSE',
+      jsOutputFile: 'elements.min.js',
+      createSourceMap: true,
+    }));
 });
