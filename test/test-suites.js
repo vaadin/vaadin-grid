1a9b4862a9ad1c5d47e3892fe3ef5e3ff6d8c3f2
Hadi Amiri <hadi@vaadin.com>
2021-11-30 11:35:08 +0200

    chore: fix test suit list

--- test/test-suites.js
+++ test/test-suites.js
@@ -11,7 +11,32 @@ window.VaadinGridSuites = [
   'column-group.html',
   'column-groups.html',
   'column-reordering.html',
-  'column-resizing.html'
+  'column-resizing.html',
+  'column.html',
+  'data-provider.html',
+  'drag-and-drop.html',
+  'dynamic-item-size.html',
+  'event-context.html',
+  'filtering.html',
+  'frozen-columns.html',
+  'hidden-grid.html',
+  'tree-toggle.html',
+  'iron-list.html',
+  'keyboard-navigation.html',
+  'lazy-import.html',
+  'light-dom-observing.html',
+  'physical-count.html',
+  'resizing.html',
+  'resizing-material.html',
+  'row-details.html',
+  'row-height.html',
+  'scroll-to-index.html',
+  'scrolling-mode.html',
+  'selecting.html',
+  'sorting.html',
+  'style-scope.html',
+  'templates.html',
+  'renderers.html'
 ];
 
 if (isPolymer2) {
