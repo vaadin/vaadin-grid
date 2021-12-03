748430e886ec725c369d5d8f2c8101f157caedf1
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-12-03 09:56:13 +0100

    fix settings script declaring global constant

--- test/settings.js
+++ test/settings.js
@@ -1,7 +1,9 @@
-const ie = !!window.MSStream;
-const ios = !ie && /iPad|iPhone|iPod/.test(navigator.userAgent);
-WCT = {
-  mochaOptions: {
-    timeout: ios || ie ? 40000 : 30000
-  }
-};
+(function() {
+  const ie = !!window.MSStream;
+  const ios = !ie && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  window.WCT = {
+    mochaOptions: {
+      timeout: ios || ie ? 40000 : 30000
+    }
+  };
+})();
